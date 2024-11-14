efb029c1b5b60f498b8881ead737e38625c7fee8
Prince-The-Billionaire <danielprincewill14@gmail.com>
2024-11-14 12:50:43 +0100

    tenth

--- javascript1.js
+++ javascript1.js
@@ -1,6 +1,6 @@
 // Code for finding the average of ten numbers
 
-let val = console.assert()
+
 
 let A = [0,1,2,3,4,5,6,7,8,9,10]
 let sum = 0;
@@ -46,7 +46,18 @@ combination(9,6)
 
 // document object manipulation
 document.getElementById("array_answer").innerText = "The average of all 10 number is "+ average
-let x = document.getElementById("x").value
-let y = document.getElementById("y").value
-let combination_value = combination(x,y)
-document.getElementById("combination_answer").innerText = "The combination of the two numbers "+ x + " "+y+ " is "+combination_value
+
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let x = document.getElementById("x").value
+    let y = document.getElementById("y").value
+    let combination_value = combination(x,y)
+    document.getElementById("combination_answer").innerText = "The combination of the two numbers "+ x + " & "+y+ " is "+combination_value
+
+    if(x > y){
+        document.getElementById("inequality_answer").innerText = "X is greater than Y"
+    }
+    else{
+        document.getElementById("inequality_answer").innerText = "X is less than Y"
+    }
+  });
